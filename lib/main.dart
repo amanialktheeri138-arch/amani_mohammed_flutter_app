2c11968cb9e865773c4b9f7c33291b8da18e1fc8
amanialktheeri138-arch <amanialktheeri138@gmail.com>
2025-12-10 11:28:37 -0800

    amaniBinAlshin

--- lib/main.dart
+++ lib/main.dart
@@ -7,114 +7,67 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'مسبحة الكترونية',
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-     
-  }
-}
+        home:Scaffold(
+            appBar: AppBar(title: Text('User Profile:Amani'),),
+            body:Column(
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                          width:400,
+                          height:190,
+                          padding: EdgeInsets.all(30),
+                          color: Colors.deepPurpleAccent,
+                          ),
+                      Positioned(
+                        top: 20,
+                        child:
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundImage: Image.asset("image/amani.jpg").image,
+                        ),),
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-  
+                      Positioned(
+                        bottom: 5,
+                        child: Column(
+                          children: [
+                            Text('Amani Mohammed',style: TextStyle(fontSize: 20,color: Colors.white),),
+                            SizedBox(height: 6,),
+                            Text('Sofftware Devloper',style: TextStyle(fontSize: 18,color: Colors.orange),) ,
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int counter1_ =0;
- int counter2_ =0;
- int counter3_ =0;
-String message1 =' ابدا';
-
-  int counter =0;
-
- 
-//دوال التحديث محتوى الرسالة والعداد
-
-  void _incrementCounter1() {
-    setState(() {
-      message1='سبحان لله';
-      counter1_++;
-      counter =counter1_;
-    });
-  }
+                          ],
+                        ) ,
+                      ),
+                    ],
+                  ),
 
 
-  void _incrementCounter2() {
-    setState(() {
-      message1=' الحمد لله';
-      counter2_++;
-      counter =counter2_;
-    });
-  }
-void _incrementCounter3() {
-    setState(() {
-      message1=' الله اكبر';
-      counter3_++;
-      counter =counter3_;
-    });
-  }
-  void reset(){
-setState(() {
-    message1 = 'ابدا ';
-    counter1_ = 0;
-    counter2_ = 0;
-    counter3_ = 0;
-    counter = 0;
-  });
 
-  }
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(title: Text('مسبحة الكترونية'),
-      centerTitle: true,
-      backgroundColor: const Color.fromARGB(202, 255, 116, 248),
-         
-       //جسم الذي يحتوي على مكونات المسبحة من ازرار ونص وعداد 
-      ),
-      body: Center(
-       
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            
-         
-            Text(message1,style: TextStyle(fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 241, 255, 51),fontSize: 20)),
-            Text("$counter" ,style: TextStyle(fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 255, 153, 0),fontSize: 20)),
-            
-            SizedBox(height: 50),
-         
-            Row(
-  mainAxisAlignment: MainAxisAlignment.center,
-  children: [
-            ElevatedButton(onPressed: _incrementCounter1, 
-            child: Text("سبحان لله",style: TextStyle(fontWeight: FontWeight.bold, color: Colors.amber,fontSize: 20))),
-             SizedBox(width: 20),
-           ElevatedButton(onPressed: _incrementCounter2, 
-           child: Text("الحمد لله",style: TextStyle(fontWeight: FontWeight.bold, color: Colors.amber,fontSize: 20),)),
-            SizedBox(width: 20),
-           ElevatedButton(onPressed: _incrementCounter3, 
-           child: Text("الله اكبر",style: TextStyle(fontWeight: FontWeight.bold, color: Colors.amber,fontSize: 20),)),]),
-            SizedBox(height: 40),
-           ElevatedButton( onPressed: reset ,child: Text("مسح",style: TextStyle(fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 185, 1, 1),fontSize: 20) ),
-),
-         
-          ] 
-           ),
-        ),
-      backgroundColor: const Color.fromARGB(143, 118, 250, 239),    
-      );
-      
-  }
-}
+                  Padding(padding:EdgeInsets.all(10.0),
+                    child:  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Email',style: TextStyle(color: Colors.indigo),),
+                        Text('amanialktheeri138@gmail.com'),
+                      ],
+                    ),),
+                  Divider(),
+                  Padding(padding: const EdgeInsets.all(10.0),
+                    child:  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Phone',style: TextStyle(color: Colors.indigo),),
+                        Text('781593861'),
+                      ],
+                    ),),
+
+
+                ]
+            ))
+    );}}

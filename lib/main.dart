158418654380c537dda3ce709257e7f78d7725f3
amanialktheeri138-arch <amanialktheeri138@gmail.com>
2025-12-10 13:18:38 -0800

    proj4

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,61 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
-  
-  runApp(ProfileApp()
-  );
-  debugShowCheckedModeBanner:false;
+  runApp(DoApp());
 }
 
-class ProfileApp extends StatefulWidget {
+class DoApp extends StatefulWidget {
   @override
-  _ProfileAppState createState() => _ProfileAppState();
+  _DoAppState createState() => _DoAppState();
 }
 
-class _ProfileAppState extends State<ProfileApp> {
-  final _formKey = GlobalKey<FormState>();
-  String _name = '';
-  String _email = '';
-  final TextEditingController _passwordController = TextEditingController();
+class _DoAppState extends State<DoApp> {
+  final List<String> _todos = [];
 
-
-  void _submitForm() {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      print('Name: $_name, Email: $_email, Password : ${_passwordController.text}');
+  void _addTodoItem(String task) {
+    if (task.isNotEmpty) {
+      setState(() {
+        _todos.add(task);
+      });
     }
   }
+  void _removeTodoItem(int index) {
+    setState(() {
+      _todos.removeAt(index);
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Form Example')),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Name'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your name';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    _name = value!;
-                  },
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Email'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    } else if (!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value!)) {
-                      return 'Please enter a valid email address';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    _email = value!;
-                  },
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    else if (value!.length<8) {
-                      return ' password should be at least 8 chars';
-                    }else if (!RegExp("[a-z]").hasMatch(value!)) {
-                      return ' password should at least one lowercase chars';
-                    }else if (!RegExp("[A-Z]").hasMatch(value!)) {
-                      return ' password should at least one uppercase chars';
-                    }else if (!RegExp("[!#\$%&'*+-/=?^_`{|}~@]").hasMatch(value!)) {
-                      return ' password should at least one symbol';
-                    }else if (!RegExp("[0-9]").hasMatch(value!)) {
-                      return ' password should at least one number';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  child: Text('Submit'),
-                ),
-              ],
+        appBar: AppBar(
+          title: Text('Simple ToDo App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            TextField(
+              onSubmitted: _addTodoItem,
+              decoration: InputDecoration(
+                labelText: 'Enter a new task',
+              ),
             ),
-          ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _todos.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_todos[index]),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () => _removeTodoItem(index),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );

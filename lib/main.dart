af1f90670ae616caa2c094f22986bced30dc0a97
amanialktheeri138-arch <amanialktheeri138@gmail.com>
2025-12-11 08:03:44 -0800

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,115 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(DoApp());
+  runApp(const MyApp());
 }
 
-class DoApp extends StatefulWidget {
-  @override
-  _DoAppState createState() => _DoAppState();
-}
-
-class _DoAppState extends State<DoApp> {
-  final List<String> _todos = [];
-
-  void _addTodoItem(String task) {
-    if (task.isNotEmpty) {
-      setState(() {
-        _todos.add(task);
-      });
-    }
-  }
-  void _removeTodoItem(int index) {
-    setState(() {
-      _todos.removeAt(index);
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Simple ToDo App'),
-        ),
-        body: Column(
-          children: <Widget>[
-            TextField(
-              onSubmitted: _addTodoItem,
-              decoration: InputDecoration(
-                labelText: 'Enter a new task',
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _todos.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(_todos[index]),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () => _removeTodoItem(index),
+        home: Scaffold(
+          appBar: AppBar(title: Text('Product Listing '),),
+          body: ListView(
+            children: [
+              Row(
+                children: [
+                  Image.asset('images/2.jpg',width:100,height: 100,),
+
+                  Expanded(
+                    child: Column(
+                      children: [
+
+                        Text('iPhone',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
+                        SizedBox(height: 10,),
+                        Text('iPhone is the slylist phone ever'),
+                        SizedBox(height: 10,),
+                        Text('price: 1000'),
+                      ],
                     ),
-                  );
-                },
+                  )
+                ],
               ),
-            ),
-          ],
-        ),
-      ),
+              Row(
+                children: [
+                  Image.asset('images/1.jpg',width:100,height: 100,),
+
+                  Expanded(
+                    child: Column(
+                      children: [
+
+                        Text('Pixel',style: TextStyle(fontWeight: FontWeight.bold),),
+                        SizedBox(height: 10,),
+                        Text('Pixel is most featureful phone ever'),
+                        SizedBox(height: 10,),
+                        Text('price: 800'),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Image.asset('images/3.jpg',width:100,height: 100,),
+
+                  Expanded(
+                    child: Column(
+                      children: [
+
+                        Text('Laptop',style: TextStyle(fontWeight: FontWeight.bold),),
+                        SizedBox(height: 10,),
+                        Text('Laptop is most productive development tool'),
+                        SizedBox(height: 10,),
+                        Text('price:2000'),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Image.asset('assets/1.jpg',width:100,height: 100,),
+
+                  Expanded(
+                    child: Column(
+                      children: [
+
+                        Text('Tablet',style: TextStyle(fontWeight: FontWeight.bold),),
+                        SizedBox(height: 10,),
+                        Text('Tablet the most useful device ever for meetinfg '),
+                        SizedBox(height: 10,),
+                        Text('price: 1500'),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Image.asset('images/4.jpg',width:100,height: 100,),
+
+                  Expanded(
+                    child: Column(
+                      children: [
+
+                        Text('Pen Drive',style: TextStyle(fontWeight: FontWeight.bold),),
+                        SizedBox(height: 10,),
+                        Text('iPhone is the slylist phone ever'),
+                        SizedBox(height: 10,),
+                        Text('price: 6000'),
+
+
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        )
+
     );
-  }
-}
+  }}

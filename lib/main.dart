2e68f7d6f394305884bb5f500dbf40848e5c2379
amanialktheeri138-arch <amanialktheeri138@gmail.com>
2025-12-11 08:11:17 -0800

    nwe2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'secand.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +8,29 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(title: Text('Product Listing '),),
-          body: ListView(
-            children: [
-              Row(
-                children: [
-                  Image.asset('images/2.jpg',width:100,height: 100,),
-
-                  Expanded(
-                    child: Column(
-                      children: [
-
-                        Text('iPhone',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
-                        SizedBox(height: 10,),
-                        Text('iPhone is the slylist phone ever'),
-                        SizedBox(height: 10,),
-                        Text('price: 1000'),
-                      ],
-                    ),
-                  )
-                ],
+    return Scaffold(
+      appBar: AppBar(title:  Text('Home Page')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async{
+            final result = await Navigator.push<bool>(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>  Secand(),
               ),
-              Row(
-                children: [
-                  Image.asset('images/1.jpg',width:100,height: 100,),
-
-                  Expanded(
-                    child: Column(
-                      children: [
-
-                        Text('Pixel',style: TextStyle(fontWeight: FontWeight.bold),),
-                        SizedBox(height: 10,),
-                        Text('Pixel is most featureful phone ever'),
-                        SizedBox(height: 10,),
-                        Text('price: 800'),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset('images/3.jpg',width:100,height: 100,),
-
-                  Expanded(
-                    child: Column(
-                      children: [
-
-                        Text('Laptop',style: TextStyle(fontWeight: FontWeight.bold),),
-                        SizedBox(height: 10,),
-                        Text('Laptop is most productive development tool'),
-                        SizedBox(height: 10,),
-                        Text('price:2000'),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset('assets/1.jpg',width:100,height: 100,),
-
-                  Expanded(
-                    child: Column(
-                      children: [
-
-                        Text('Tablet',style: TextStyle(fontWeight: FontWeight.bold),),
-                        SizedBox(height: 10,),
-                        Text('Tablet the most useful device ever for meetinfg '),
-                        SizedBox(height: 10,),
-                        Text('price: 1500'),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset('images/4.jpg',width:100,height: 100,),
-
-                  Expanded(
-                    child: Column(
-                      children: [
-
-                        Text('Pen Drive',style: TextStyle(fontWeight: FontWeight.bold),),
-                        SizedBox(height: 10,),
-                        Text('iPhone is the slylist phone ever'),
-                        SizedBox(height: 10,),
-                        Text('price: 6000'),
-
-
-                      ],
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-        )
-
+            );
+            if (result != null) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(result ?'تمت الموافقة على الشروط':'تم رفض الشروط')),
+              );
+            }
+          },
+          child:  Text('Go to selection screen'),
+        ),
+      ),
     );
-  }}
+  }
+
+}

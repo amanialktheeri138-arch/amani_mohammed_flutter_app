e6e83fe0caa27532edc3726924730b2274119d33
amanialktheeri138-arch <amanialktheeri138@gmail.com>
2025-12-11 09:12:16 -0800

    new5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'secand_.dart';
-import 'about.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,50 +10,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Named Routes Demo',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomeScreen(),
-        '/Second': (context) =>  Secand(),
-        '/about': (context) =>  About(),
-      },
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
+      appBar: AppBar(title:  Text('Home Page')),
       body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () async{
-                final result = await Navigator.pushNamed(
-                    context,'/Second'
-                )as bool?;
-                if (result != null) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text(result?'You selected accept':'you selected not to accept')),
-                  );
-                }
-              },
-              child: const Text('Go to selection screen'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/about');
-              },
-              child: const Text('Go to About'),
-            ),
-          ],
+        child: ElevatedButton(
+          onPressed: () async{
+            final result = await Navigator.push<bool>(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>  Secand(),
+              ),
+            );
+            if (result != null) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(result ?'تمت الموافقة على الشروط':'تم رفض الشروط')),
+              );
+            }
+          },
+          child:  Text('Go to selection screen'),
         ),
       ),
     );
   }
+
 }
